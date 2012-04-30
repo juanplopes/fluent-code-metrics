939ff53c9ae3526d88478dfffff782f9b899d95c
elemarjr <elemarjr@gmail.com>
2012-04-30 14:26:10 -0300

    Failing Acceptance Test

---
+++ src/FluentCodeMetrics.Core/CeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentCodeMetrics.Core
+{
+    public static class CeExtensions
+    {
+        public static int ComputeCe(this Type that)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FluentCodeMetrics.Specs/EfferentCouplingSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentCodeMetrics.Core;
+using SharpTestsEx;
+using TechTalk.SpecFlow;
+
+namespace FluentCodeMetrics.Specs
+{
+    [Binding]
+    public class EfferentCouplingSteps
+    {
+        // ReSharper disable InconsistentNaming
+        private int resultingCe;
+        private Type workingType;
+        // ReSharper restore InconsistentNaming
+
+        [Given(@"que tenho um (.*)")]
+        public void DadoQueTenhoUm(string tipo)
+        {
+            workingType = Type.GetType(tipo);
+            workingType.Should().Not.Be.Null();
+        }
+
+        [When(@"inspeciono seu acoplamento eferente")]
+        public void QuandoInspecionoSeuAcoplamentoEferente()
+        {
+            resultingCe = workingType.ComputeCe();
+        }
+
+        [Then(@"obtenho (.*)")]
+        public void EntaoObtenho(int ce)
+        {
+            resultingCe.Should().Be(ce);
+        }
+    }
+}
