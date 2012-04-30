6c40a247563f9e09b633743fd9b2fcbb93aed7df
elemarjr <elemarjr@gmail.com>
2012-04-30 13:18:30 -0300

    Ce Feature

---
+++ src/FluentCodeMetrics.Specs/EfferentCoupling.feature
@@ -0,0 +1,28 @@
+﻿#language: pt-BR
+
+Funcionalidade: Calcular Acoplamento Eferente (Ce)
+	
+	Acomplamento Eferente é uma métrica que indica a quantidade
+	de referências de um tipo para outros.
+	Entram na contagem: classe base, interfaces implementadas, 
+	tipos dos variáveis locais, atributos e das propriedades, 
+	tipos dos parâmetros em métodos e construtores, exceptions.
+
+	Esquema do Cenário: Calcular Acoplamento Eferente para um tipo
+		Dado que tenho um <tipo>
+		Quando inspeciono seu acoplamento eferente
+		Então obtenho <ce>
+
+		Exemplos: 
+			| tipo                       | ce |
+			| Samples.EmptyClass         | 1  |
+			| Samples.OneArgCtor         | 2  |
+			| Samples.OneArgVoidMethod   | 2  |
+			| Samples.IntMethod          | 2  |
+			| Samples.StringArgIntMethod | 3  |
+			| Samples.OneProperty        | 2  |
+			| Samples.OneAttribute       | 2  |
+			| Samples.OneException       | 2  |
+
+
+
---
+++ src/FluentCodeMetrics.Specs/Samples.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+// ReSharper disable CheckNamespace
+namespace Samples
+// ReSharper restore CheckNamespace
+{
+    public class EmptyClass
+    {
+    }
+
+    class OneArgCtor
+    {
+// ReSharper disable UnusedParameter.Local
+        public OneArgCtor(int arg)
+// ReSharper restore UnusedParameter.Local
+        {}
+    }
+
+    class OneArgVoidMethod
+    {
+         public void Foo(int arg)
+         {}
+    }
+
+    class IntMethod
+    {
+        public int Foo()
+        {
+            return 0;
+        }
+    }
+
+    class StringArgIntMethod
+    {
+         public int Foo(string arg)
+         {
+             return 0;
+         }
+    }
+
+    class OneProperty
+    {
+        public string Foo { get; set; }
+    }
+
+    class OneAttribute
+    {
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+        private string foo;
+// ReSharper restore UnusedMember.Local
+// ReSharper restore InconsistentNaming
+    }
+
+    class OneException
+    {
+         public void Foo()
+         {
+             throw new Exception();
+         }
+    }
+}
